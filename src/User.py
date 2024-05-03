8419fb2fe14064de9e52eb7e19eb2dc2f61ead5a
Jason Martin <jasonbm2@uci.edu>
2024-05-03 13:06:12 -0700

    Created skeleton of User class

---
+++ src/User.py
@@ -0,0 +1,29 @@
+# User.py
+# Ojos Project
+# 
+# Contains user-related material
+
+class User():
+    def __init__(self, type_of_user):
+        self.type = type_of_user
+
+    def verify_pin(pin : int) -> bool:
+        '''
+        Verifies that the inputted pin is valid
+        '''
+        pass
+    
+    def set_user_type(new_user_type : str) -> None:
+        self.type = new_user_type
+
+    def get_patient_medications() -> Medication[]:
+        '''
+        Returns the patient's list of Medications
+        '''
+        pass
+    
+    def get_care_instructions() -> str[]:
+        '''
+        Returns a list of care instructions pertaining to the Patient
+        '''
+        pass
