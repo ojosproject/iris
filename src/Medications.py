371dce66426c27225de46557d018d51c7b40f49b
Jason Martin <jasonbm2@uci.edu>
2024-05-03 12:59:24 -0700

    Started Medication class

--- src/Medications.py
+++ src/Medications.py
@@ -2,3 +2,23 @@
 # Ojos Project
 # 
 # This file will include medication-related material.
+
+from main import DB
+
+def get_medications():
+    pass
+
+class Medication:
+    def __init__(self, name : str, brand : str, dosage : float):
+        self.name = name
+        self.brand = brand
+        self.dosage = dosage
+    
+    def taken() -> bool:
+        pass
+
+    def log() -> None:
+        pass
+
+    def change_dosage(new_dosage : float) -> None:
+        self.dosage = new_dosage
