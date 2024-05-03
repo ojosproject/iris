db5284f4f7acca25bcd2c2480dfd86e1afd486c4
Jason Martin <jasonbm2@uci.edu>
2024-05-03 14:26:29 -0700

    feat: created drafts
    Created rough drafts for Medication, User, and Database modules. Testing is needed for database calls, etc.
    
    Co-authored-by: Carlos Valdez <calejvaldez@users.noreply.github.com>

--- src/User.py
+++ src/User.py
@@ -2,30 +2,41 @@
 # Ojos Project
 # 
 # Contains user-related material
-
-import Medication
+from main import DB
+from Medications import Medication
 
 class User():
-    def __init__(self, type_of_user):
-        self.type = type_of_user
+    def __init__(self, pin: int):
+        self._type = self.verify_pin(pin)
 
-    def verify_pin(pin : int) -> bool:
+        if self._type == None:
+            raise Exception("The pin is not valid.")
+
+    def verify_pin(self, pin : int) -> str:
         '''
         Verifies that the inputted pin is valid
+
+        Return the type, or None.
+        if it's None, it's not a valid pin
         '''
         pass
-    
-    def set_user_type(new_user_type : str) -> None:
-        self.type = new_user_type
 
-    def get_patient_medications() -> Medication[]:
+    @property
+    def type(self) -> str:
+        return self._type
+
+    def get_patient_medications() -> list[Medication]:
         '''
         Returns the patient's list of Medications
         '''
-        return Medication.get_medications()
+        return Medication.get_all_medications()
     
-    def get_care_instructions() -> str[]:
+    def get_care_instructions(self) -> list[str]:
         '''
         Returns a list of care instructions pertaining to the Patient
         '''
         pass
+
+class Patient(User):
+    def __init__(self, type_of_user):
+        super().__init__(type_of_user)
--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@
 # This is the file where it all begins.
 from Database import Database
 
-DB = Database("some path")
+DB = Database("database.db")
 
 if __name__ == "__main__":
     print("No code added yet!")
