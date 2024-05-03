09a2354529cfb02db0c83b081e34dfba80c1b8e9
Carlos Valdez <cvaldezh@uci.edu>
2024-05-03 12:03:42 -0700

    feat(main): added global database instance

--- src/main.py
+++ src/main.py
@@ -2,5 +2,9 @@
 # Ojos Project
 # 
 # This is the file where it all begins.
+from Database import Database
+
+DB = Database("some path")
+
 if __name__ == "__main__":
     print("No code added yet!")
