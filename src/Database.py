372d47e33c4e9f502a132c9549bfb30941bb2b7d
Jason Martin <jasonbm2@uci.edu>
2024-05-09 14:40:47 -0700

    feat: added methods to modify medication table
    Created add_medication(), del_medication(), and wrote tests for them
    
    Co-authored-by: Jesse David <jessed7@users.noreply.github.com>
    Co-authored-by: AyushJain2021 <AyushJain2021@users.noreply.github.com>

--- src/Database.py
+++ src/Database.py
@@ -11,33 +11,41 @@ class Database:
     def __init__(self, path_to_db: str) -> None:
         self._connection = sqlite3.connect(path_to_db)
 
-    def get_medications(self, include_only: list[str] = []) -> list[dict]:
-        # todo: need to include the `include_only` feature
+    def get_medications(self, include_only: tuple[str] = ()) -> tuple[dict]:
+        #TODO: need to include the `include_only` feature
         """Return all medications in the database unless `include_only` is specified.
 
         Args:
-            include_only (list[str], optional): A list of strings with the name of the medication. Defaults to [].
+            include_only (tuple[str], optional): A immutable tuples of strings with the name of the medication. Defaults to ().
 
         Returns:
-            list: Returns a list of dictionaries that represent
+            tuple: Returns a tuple of dictionaries where each value represents a medication object
         """
-        list_to_return = []
+        tuple_to_return = []
+
+            
+
 
         with self._connection as db:
+
             cursor = db.execute("SELECT * FROM medication")
 
             for medication in cursor.fetchall():
-                list_to_return.append({"name": medication[0], "brand": medication[1], "dose": float(medication[2]), "supply": float(
-                    medication[3]), "first_added": float(medication[4]), "last_token": float(medication[5])})
+                tuple_to_return.append({"name": medication[0], "brand": medication[1], "dose": medication[2], "supply": int(
+                    medication[3]), "first_added": int(medication[4]), "last_taken": int(medication[5])})
 
-        return list_to_return
+            if include_only: # if there are specific medication(s) that the user wants to find
+                cursor = db.execute8
 
-    def set_medication_dosage(self, name: str, dosage: float) -> None:
+
+        return tuple_to_return
+
+    def set_medication_dose(self, name: str, dose: float) -> None:
         with self._connection as db:
             # todo: run tests
             db.execute(
-                'UPDATE medication SET dosage = :dosage WHERE name = :name', {
-                    'dosage': dosage, 'name': name}
+                'UPDATE medication SET dose = :dose WHERE name = :name', {
+                    'dose': dose, 'name': name}
             )
 
             db.commit()
@@ -52,5 +60,24 @@ class Database:
 
             db.commit()
 
-    def add_medication(self) -> None:
-        ...
+    def add_medication(self, name: str, brand: str, dose: int, supply: int, first_added: int, last_taken: int) -> None:
+        with self._connection as db:
+            # todo: run tests
+            db.execute(
+                '''INSERT INTO medication (name, brand, dose, supply, first_added, last_taken)
+                    VALUES (:name, :brand, :dose, :supply, :first_added, :last_taken)''',
+                {'name': name, 'brand': brand, 'dose': dose, 'supply': supply,
+                    'first_added': first_added, 'last_taken': last_taken}
+            )
+
+            db.commit()
+
+    def del_medication(self, name: str) -> None:
+        with self._connection as db:
+            # todo: run tests
+            db.execute(
+                'DELETE FROM medication WHERE name = ?',
+                (name,)
+            )
+
+            db.commit()
