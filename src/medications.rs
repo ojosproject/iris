aa86349802060af6d02ad303178e78a354f823a4
Carlos Valdez <cvaldezh@uci.edu>
2024-07-19 17:01:44 -0700

    feat(medications): added Medication struct

--- src/medications.rs
+++ src/medications.rs
@@ -2,3 +2,12 @@
 // Ojos Project
 // 
 // This handles a lot of medication-related functions.
+pub struct Medication {
+    name: String,
+    brand: String,
+    dosage: f64,
+    frequency: Option<String>,
+    supply: Option<f64>,
+    first_added: Option<f64>,
+    last_taken: Option<f64>,
+}
