b7036bdaa64b78e564475ab398bdc42d895887e2
Carlos Valdez <cvaldezh@uci.edu>
2024-07-22 15:55:50 -0700

    feat(database): now using impl and struct
    This makes it so that, when we use this code, it feels more OOP-y.
    
    Co-authored-by: Jason Martin <jasonbm2@uci.edu>
    Co-authored-by: Jesse David <jessecd@uci.edu>

--- src/database.rs
+++ src/database.rs
@@ -5,67 +5,112 @@
 // Use this for reference: https://github.com/ojosproject/iris/tree/python
 use rusqlite::{Connection, Result};
 
-static CONNECTION: Option<String> = None;
-static MEDICATION_CACHE: Option<Vec<String>> = None;
-static LAST_CACHED: Option<f64> = None;
-
-// add your name
-fn med_in_db(name: &str) -> bool {
-    // todo: implement function
-    true
+pub struct Database {
+    connection: Connection,
+    medication_cache: Vec<String>,
+    last_cached: f64,
 }
 
-fn update_cache() {
-    // todo: implement function
-}
+impl Database {
+    pub fn new(path_to_db: &str) -> Database {
+        let connection = Connection::open(path_to_db).expect("An error occurred.");
+        Database {
+            connection,
+            medication_cache: vec![], //this creates an empty vector
+            last_cached: 0.0,         // i'm not sure how we're implementing time
+        }
+    }
 
-pub fn get_medications(include_only: Vec<String>, force_update_cache: bool) -> Vec<String> {
-    // todo: implement function
-    vec!["".to_string()]
-}
+    // Jason
+    pub fn med_in_db(&mut self, name: &str) -> bool {
+        let mut stmt = self
+            .connection
+            .prepare("SELECT name FROM medication WHERE name = (name)", name);
+        let mut rows = stmt.query(params![name]);
 
-pub fn set_medication_dose(name: &str, dose: &f64) {
-    // todo: implement function
-}
+        true
+    }
 
-pub fn set_medication_supply(name: &str, supply: f64) {
-    // todo: implement function
-}
+    // Jesse
+    pub fn add_medication(
+        &mut self,
+        name: &str,
+        brand: &str,
+        dose: i64,
+        supply: i64,
+        first_added: i64,
+        last_taken: i64,
+        frequency: Option<String>,
+    ) -> Result<()> {
+        // todo: implement function
+        // assuming connection is initialized properly
 
-// Jesse
-pub fn add_medication(name: &str, brand: &str, dose: i64, supply: i64, first_added: i64, last_taken: i64, frequency: Option<String>) -> Result<()>{
-    // todo: implement function
-    // assuming connection is initialized properly
-    
-    // Change to using the actual connection, but for now, using a connection in memory
-    let conn = Connection::open_in_memory()?;
+        // Change to using the actual connection, but for now, using a connection in memory
+        // let conn = Connection::open_in_memory()?;
 
-    conn.execute(
-        "INSERT INTO medication (name, brand, dose, frequency, supply, first_added, last_taken)
+        self.connection.execute(
+            "INSERT INTO medication (name, brand, dose, frequency, supply, first_added, last_taken)
         VALUES (?name, ?brand, ?dose, ?frequency, ?supply, ?first_added, ?last_taken)",
-                (name, brand, dose, supply, first_added, last_taken, frequency)
-    )?;
+            (
+                name,
+                brand,
+                dose,
+                supply,
+                first_added,
+                last_taken,
+                frequency,
+            ),
+        )?;
 
-    // In the Python version we called a function to commit the change to the db and close the cursor that was returned by the execute function
-    // Is this not relevant to this version in rust?
+        // In the Python version we called a function to commit the change to the db and close the cursor that was returned by the execute function
+        // Is this not relevant to this version in rust?
 
-    Ok(())
-}
+        Ok(())
+    }
 
-// Jesse
-pub fn del_medication(name: &str) -> Result<()> {
-    // todo: implement function
-    // Change to using the actual connection, but for now, using a connection in memory
-    let conn = Connection::open_in_memory()?;
+    // Jesse
+    pub fn del_medication(&mut self, name: &str) -> Result<()> {
+        // todo: implement function
+        // Change to using the actual connection, but for now, using a connection in memory
+        // let conn = Connection::open_in_memory()?;
 
-    conn.execute(
-        "DELETE FROM medication WHERE name = ?name", (name,)
-    )?;
+        self.connection
+            .execute("DELETE FROM medication WHERE name = ?name", (name,))?;
 
-    Ok(())
-}
+        Ok(())
+    }
 
-pub fn log_medication(name: &str, dosage: &str, comments: Option<String>) -> f64 {
-    // todo: implement function
-    0.0
+    // Jesse
+    pub fn set_medication_dose(&mut self, name: &str, dose: &f64) -> Result<()> {
+        // todo: add check to see if the medication name is in the DB, and raise error if not
+        self.connection.execute(
+            "UPDATE medication SET dose = ?dose WHERE name = ?name",
+            (dose, name),
+        )?;
+
+        Ok(())
+    }
+
+    // Jesse
+    pub fn set_medication_supply(&mut self, name: &str, supply: f64) -> Result<()> {
+        // todo: add check to see if the medication name is in the DB, and raise error if not
+        self.connection.execute(
+            "UPDATE medication SET supply = ?supply WHERE name = ?name",
+            (supply, name),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn get_medications(include_only: Vec<String>, force_update_cache: bool) -> Vec<String> {
+        // todo: implement function
+        vec!["".to_string()]
+    }
+
+
+
+    pub fn log_medication(name: &str, dosage: &str, comments: Option<String>) -> f64 {
+        // todo: implement function
+        0.0
+    }
 }
