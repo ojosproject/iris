274e17cb31fb48c6e41d0a5d533c3ae4daae46c6
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-08-02 14:57:55 -0700

    Added clock to hub

--- src/app/components/HubHeader.tsx
+++ src/app/components/HubHeader.tsx
@@ -1,12 +1,14 @@
 "use client";
 import classes from "./HubHeader.module.css";
 import { useState } from "react";
+import Clock from 'react-live-clock';
 
 export default function HubHeader() {
   // todo: Possibly change these from useState to fetching from the backend for the user name and current time of day
   const [userName, setUserName] = useState("Name");
   const [timeOfDay, setTimeOfDay] = useState("morning");
   // todo: get current time
+  // todo: change formatting in Clock element to have multiple lines
 
   return (
     <header className={classes.head}>
@@ -19,7 +21,10 @@ export default function HubHeader() {
         </h1>
       </div>
       <div className={classes.time}>
-        <p>insert current time</p>
+        {/* <p>insert current time</p> */}
+        <Clock format={"h:mm A"} ticking className={classes.hourMin} />
+        <Clock format={"MMM D, YYYY"} ticking className={classes.date} />
+        <Clock format={"dddd"} ticking className={classes.date} />
       </div>
     </header>
   );
