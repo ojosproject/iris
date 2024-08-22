12c9cb1ec7af078db5491dadfbd7c995aa3b3539
Carlos Valdez <cvaldezh@uci.edu>
2024-08-21 19:54:54 -0700

    chore: started invoke command, though it requires a credential
    There's no way for frontend to request a user be created nor take
    credential input. Without a credential, this function will fail.

--- src/app/components/UpcomingList.tsx
+++ src/app/components/UpcomingList.tsx
@@ -1,26 +1,38 @@
+"use client"; // added for useState(). is there another way?
+
+import { useEffect, useState } from "react";
 import classes from "./UpcomingList.module.css";
 import UpcomingMed from "./UpcomingMed";
-
-// todo: add fetching for the list of upcoming medicines
-// for now, using a hard-coded array of data
-type UpcomingMedData = {
-  name: string;
-  time: string;
-};
-
-const upcomingMeds: UpcomingMedData[] = [
-  { name: "Med 1", time: "10:00 AM" },
-  { name: "Med 2", time: "2:00 PM" },
-  { name: "Med 3", time: "3:30 PM" },
-];
+import { Medication } from "@/types";
+import { invoke } from "@tauri-apps/api/core";
 
 export default function UpcomingList() {
+  const [upcomingMeds, setUpcomingMeds] = useState([] as Medication[]);
+
+  useEffect(() => {
+    // https://v2.tauri.app/develop/calling-rust/
+    // ! the command requires a security credential
+    // ! this will fail if no user inside the database exists with this credential
+    // todo: implement a way to request a user be created
+    invoke("get_upcoming_medications", { credential: null })
+      .then((d) => {
+        setUpcomingMeds(d as Medication[]);
+      })
+      .catch((e) => {
+        // todo: can we create a way for errors to be displayed on screen?
+        console.log(e);
+      });
+  }, []);
+
   return (
     <ul className={classes.medList}>
       {upcomingMeds.map((med) => {
         return (
           <li>
-            <UpcomingMed time={med.time} name={med.name}></UpcomingMed>
+            <UpcomingMed
+              time={med.upcoming_dose!} // guaranteed to have a value. if not, let backend know
+              name={med.name}
+            ></UpcomingMed>
           </li>
         );
       })}
--- src/app/components/UpcomingMed.tsx
+++ src/app/components/UpcomingMed.tsx
@@ -1,14 +1,21 @@
 import classes from "./UpcomingMed.module.css";
 
 type UpcomingMedProps = {
-  time: string; // Maybe make it a number?
+  time: number; // in Epoch seconds
   name: string;
 };
+
+function secondsToStringTime(epoch: number): string {
+  let epoch_string = new Date(epoch * 1000);
+  let hour = epoch_string.getHours();
+
+  return `${hour > 12 ? hour - 12 : hour}:${epoch_string.getMinutes()} ${hour > 12 ? "AM" : "PM"}`;
+}
 
 export default function UpcomingMed({ time, name }: UpcomingMedProps) {
   return (
     <div className={classes.med}>
-      <p> {time} </p>
+      <p> {secondsToStringTime(time)} </p>
       <h3> {name} </h3>
     </div>
   );
