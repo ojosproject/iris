61b072f417ad943d35ba423d4cf8a151909f7d7e
Helen <helenkachau@gmail.com>
2024-10-24 13:53:09 -0700

    feat(page.tsx): added back button

--- src/app/pro/page.tsx
+++ src/app/pro/page.tsx
@@ -1,11 +1,16 @@
 "use client"
-import "./survey.css";
+import "./pro.css";
 import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Main_page() {
     const router = useRouter();
     return (
     <>
+        <button onClick={() => router.back()}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
         <h1>
             PROs
         </h1>
