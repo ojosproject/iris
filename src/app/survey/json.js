4afa927778c01cccb5bc88ce423f87dfaaecf46d
Helen <helenkachau@gmail.com>
2024-11-05 13:55:56 -0800

    feat(survey): css and temporary database added

---
+++ src/app/survey/json.js
@@ -0,0 +1,46 @@
+export const json = {
+    "pages": [
+    {
+        "name": "page1",
+        "elements": [
+        {
+            "type": "rating",
+            "name": "satisfaction-numeric-1",
+            "title": "How satisfied are you with our product?",
+            "autoGenerate": false,
+            "rateCount": 10,
+            "rateValues": [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+            "scaleColorMode": "colored"
+        },
+        {
+            "type": "rating",
+            "name": "satisfaction-numeric-2",
+            "title": "How satisfied are you with our product?",
+            "autoGenerate": false,
+            "rateCount": 10,
+            "rateValues": [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+            "scaleColorMode": "colored"
+        },
+        {
+            "type": "rating",
+            "name": "satisfaction-numeric-3",
+            "title": "How satisfied are you with our product?",
+            "autoGenerate": false,
+            "rateCount": 10,
+            "rateValues": [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+            "scaleColorMode": "colored"
+        },
+        {
+            "type": "rating",
+            "name": "satisfaction-numeric-4",
+            "title": "How satisfied are you with our product?",
+            "autoGenerate": false,
+            "rateCount": 10,
+            "rateValues": [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+            "scaleColorMode": "colored"
+        },
+        ]
+    }
+    ],
+    "showQuestionNumbers": "on"
+};
--- src/app/survey/page.tsx
+++ src/app/survey/page.tsx
@@ -1,18 +1,32 @@
-"use client"
+"use client";
 import { useRouter } from "next/navigation";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { Survey as SurveyComponent } from "survey-react"; 
+import { SurveyModel } from "survey-core"; 
+import { json } from "./json"; // Your survey JSON data
+import "./survey.css"; // Ensure this path is correct
 
-export default function Survey() {
+const SurveyPage = () => {
     const router = useRouter();
+    const survey = new SurveyModel(json);
+
+    // Add a handler for when the survey is completed
+    survey.onComplete.add((sender) => {
+        console.log(JSON.stringify(sender.data, null, 3));
+    });
+
     return (
-    <>
+        <>
         <button onClick={() => router.back()}>
             <FontAwesomeIcon icon={faArrowLeft} />
         </button>
-        <h1>
-            Today's Survey
-        </h1>
-    </>
+        <h1>Today's Survey</h1>
+        <div className="container">
+            {survey && <SurveyComponent model={survey} />}
+        </div>
+        </>
     );
 }
+
+export default SurveyPage;
