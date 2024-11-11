ddfccd2b79b67845a245026f027652bc48b0b720
Carlos Valdez <cvaldezh@uci.edu>
2024-11-10 16:41:05 -0800

    chore(prettier): run on TypeScript files

--- src/helper.ts
+++ src/helper.ts
@@ -1,7 +1,20 @@
-export const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+export const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export function timestampToString(timestamp: number) {
-    let d = new Date(timestamp*1000);
+  let d = new Date(timestamp * 1000);
 
-    return `${MONTHS[d.getMonth()].slice(0, 3)} ${d.getDate()}, ${d.getFullYear()}`
+  return `${MONTHS[d.getMonth()].slice(0, 3)} ${d.getDate()}, ${d.getFullYear()}`;
 }
