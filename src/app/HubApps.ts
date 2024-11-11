39a4f21b5f63d90be0e0432bd8df98889df6f9d9
Carlos Valdez <cvaldezh@uci.edu>
2024-11-10 16:42:25 -0800

    chore(HubApps.ts): remove incomplete hub apps

--- src/app/HubApps.ts
+++ src/app/HubApps.ts
@@ -2,16 +2,6 @@ import { HubAppProps } from "./components/HubApp";
 
 export const HubApps: HubAppProps[] = [
   {
-    link: "./log_view",
-    name: "Medication Logging",
-    icon: "/images/icon_pill.png",
-  },
-  {
-    link: "./call",
-    name: "Video Call",
-    icon: "/images/icon_video.png",
-  },
-  {
     link: "./resources",
     name: "Resources",
     icon: "/images/icon_resources.png",
