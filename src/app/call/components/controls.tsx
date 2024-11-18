c475b381c5307f0390ed5775458b87f8b78b87b1
Carlos Valdez <cvaldezh@uci.edu>
2024-11-17 18:30:26 -0800

    chore(controls.tsx): ran prettier

--- src/app/call/components/controls.tsx
+++ src/app/call/components/controls.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'; // Import necessary hooks and components from React
+import React, { useEffect, useRef, useState } from "react"; // Import necessary hooks and components from React
 import clsx from "clsx"; // Import clsx for conditional class names
 import classes from "./controls.module.css"; // Import CSS module for styling
-import { useRouter } from 'next/navigation';
-import Dialog from './confirmMessage';
+import { useRouter } from "next/navigation";
+import Dialog from "./confirmMessage";
 
 const WebcamRecorder: React.FC = () => {
   // Reference to the video element
@@ -21,7 +21,9 @@ const WebcamRecorder: React.FC = () => {
   const confirmDialog = () => {
     stopRecording(); // Stop recording
     if (mediaRecorderRef.current) {
-      mediaRecorderRef.current.stream.getTracks().forEach(track => track.stop());
+      mediaRecorderRef.current.stream
+        .getTracks()
+        .forEach((track) => track.stop());
     }
     setShowDialog(false); // Close the dialog
     router.push("/"); // Navigate to home page
@@ -59,7 +61,9 @@ const WebcamRecorder: React.FC = () => {
     return () => {
       // Cleanup function to stop all media tracks when component unmounts
       if (mediaRecorderRef.current) {
-        mediaRecorderRef.current.stream.getTracks().forEach(track => track.stop());
+        mediaRecorderRef.current.stream
+          .getTracks()
+          .forEach((track) => track.stop());
       }
     };
   }, []); // Empty dependency array means this runs once on component mount
@@ -68,7 +72,7 @@ const WebcamRecorder: React.FC = () => {
   const handleDataAvailable = (event: BlobEvent) => {
     if (event.data.size > 0) {
       // Append recordings if it has data
-      setRecordedChunks(prev => [...prev, event.data]);
+      setRecordedChunks((prev) => [...prev, event.data]);
     }
   };
 
@@ -91,11 +95,11 @@ const WebcamRecorder: React.FC = () => {
 
   // Function to download the recorded video
   const downloadVideo = () => {
-    const blob = new Blob(recordedChunks, { type: 'video/webm' }); // Create a Blob from the recorded chunks
+    const blob = new Blob(recordedChunks, { type: "video/webm" }); // Create a Blob from the recorded chunks
     const url = URL.createObjectURL(blob); // Create a URL for the Blob
-    const a = document.createElement('a'); // Create an anchor element
+    const a = document.createElement("a"); // Create an anchor element
     a.href = url; // Set the href to the Blob URL
-    a.download = 'recording.webm'; // Set the download filename
+    a.download = "recording.webm"; // Set the download filename
     a.click(); // Programmatically click the anchor to trigger the download
     URL.revokeObjectURL(url); // Cleanup the URL object
   };
@@ -103,7 +107,7 @@ const WebcamRecorder: React.FC = () => {
   const toggleMute = () => {
     if (mediaRecorderRef.current?.stream) {
       const audioTracks = mediaRecorderRef.current.stream.getAudioTracks();
-      audioTracks.forEach(track => {
+      audioTracks.forEach((track) => {
         track.enabled = !isMuted; // Toggle audio track enabled state
       });
       console.log("muted state: ", isMuted);
@@ -114,7 +118,7 @@ const WebcamRecorder: React.FC = () => {
   const toggleCamera = () => {
     if (mediaRecorderRef.current?.stream) {
       const videoTracks = mediaRecorderRef.current.stream.getVideoTracks();
-      videoTracks.forEach(track => {
+      videoTracks.forEach((track) => {
         track.enabled = !isCameraOn; // Toggle video track enabled state
       });
       setIsCameraOn(!isCameraOn); // Update camera state
@@ -126,51 +130,63 @@ const WebcamRecorder: React.FC = () => {
     if (recording) {
       setShowDialog(!showDialog);
     } else {
-      router.push("/")
+      router.push("/");
     }
   };
 
   return (
     <div>
       {/* Video element to display the webcam feed */}
-      <video ref={videoRef} autoPlay playsInline width="900" height="700"></video>
+      <video
+        ref={videoRef}
+        autoPlay
+        playsInline
+        width="900"
+        height="700"
+      ></video>
       <div className={classes.controls}>
         {/* Button to go back to the previous page */}
-        <button
-          className={clsx(classes.normalButton)}
-          onClick={handleEnd}
-        >
+        <button className={clsx(classes.normalButton)} onClick={handleEnd}>
           Go Back
         </button>
         {showDialog && (
-        <Dialog 
-          title="You are still recording!"
-          content="Current recording will be lost and cannot be recovered."
-          onClose={closeDialog}
-          onConfirm={confirmDialog}
-        />
+          <Dialog
+            title="You are still recording!"
+            content="Current recording will be lost and cannot be recovered."
+            onClose={closeDialog}
+            onConfirm={confirmDialog}
+          />
         )}
         {/* Conditional button to start or stop recording */}
         {!recording ? (
-          <button className ={clsx(classes.normalButton)} onClick={startRecording}>Start Recording</button>
+          <button
+            className={clsx(classes.normalButton)}
+            onClick={startRecording}
+          >
+            Start Recording
+          </button>
         ) : (
-          <button className ={clsx(classes.normalButton)} onClick={stopRecording}>Stop Recording</button>
+          <button
+            className={clsx(classes.normalButton)}
+            onClick={stopRecording}
+          >
+            Stop Recording
+          </button>
         )}
-        <button
-          className={clsx(classes.normalButton)}
-          onClick={toggleCamera}
-        >
-        {isCameraOn ? "Turn Camera Off" : "Turn Camera On"}
+        <button className={clsx(classes.normalButton)} onClick={toggleCamera}>
+          {isCameraOn ? "Turn Camera Off" : "Turn Camera On"}
         </button>
-        <button
-          className={clsx(classes.normalButton)}
-          onClick={toggleMute}
-        >
-        {isMuted ? "Mute" : "Unmute"}
+        <button className={clsx(classes.normalButton)} onClick={toggleMute}>
+          {isMuted ? "Mute" : "Unmute"}
         </button>
         {/* Button to download the video if there are recorded chunks */}
         {recordedChunks.length > 0 && (
-          <button className ={clsx(classes.normalButton)} onClick={downloadVideo}>Download Video</button>
+          <button
+            className={clsx(classes.normalButton)}
+            onClick={downloadVideo}
+          >
+            Download Video
+          </button>
         )}
       </div>
     </div>
