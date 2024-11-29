df90de8d725185df632f952dd2fa9905f27a8cc1
Carlos Valdez <cvaldezh@uci.edu>
2024-11-29 15:53:26 -0800

    chore(call): change name of video files
    <TIMESTAMP SECONDS>.mp4

--- src/app/call/helper.ts
+++ src/app/call/helper.ts
@@ -1,22 +1,19 @@
 import { BaseDirectory, exists, writeFile, mkdir } from "@tauri-apps/plugin-fs";
 
 export function saveVideo(blob: Blob) {
+  const date = new Date();
+  const fileName = `${Math.round(date.getTime() / 1000)}.mp4`;
+
   blob.arrayBuffer().then((buffer) => {
     exists("recordings/", { baseDir: BaseDirectory.AppLocalData }).then(
       (doesExist) => {
         if (!doesExist) {
           mkdir("recordings/", { baseDir: BaseDirectory.AppLocalData });
         }
-        writeFile(`recordings/${setFileName()}.mp4`, new Uint8Array(buffer), {
+        writeFile(`recordings/${fileName}`, new Uint8Array(buffer), {
           baseDir: BaseDirectory.AppLocalData,
         });
       },
     );
   });
-}
-
-export function setFileName(): string {
-  let date = new Date();
-
-  return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}_${date.getHours()}:${date.getMinutes() < 9 ? `0${date.getMinutes()}` : date.getMinutes()}:${date.getSeconds()}`;
 }
