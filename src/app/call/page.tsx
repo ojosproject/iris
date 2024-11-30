54b0787def3eabed69e9fc5a566534dfccdf39fa
Carlos Valdez <cvaldezh@uci.edu>
2024-11-30 12:09:04 -0800

    chore(call): delete camera.tsx and related files
    I think we could've used less files.

--- src/app/call/page.tsx
+++ src/app/call/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import classes from "./page.module.css";
-import Camera from "./components/camera";
 import Button from "../core/components/Button";
 import { invoke } from "@tauri-apps/api/core";
+import WebcamRecorder from "./components/controls";
 
 export default function Call() {
   return (
@@ -17,7 +17,7 @@ export default function Call() {
         />
       </header>
       <main>
-        <Camera />
+        <WebcamRecorder />
       </main>
     </>
   );
