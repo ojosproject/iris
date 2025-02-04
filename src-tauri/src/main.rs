dc3f52a424520de61b9bb192188af657c42a55e9
Carlos Valdez <cvaldezh@uci.edu>
2025-02-04 10:51:50 -0800

    fix(call): save recordings on Windows
    Closes #72

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,7 @@ use tauri::Manager;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_fs::init())
         .invoke_handler(tauri::generate_handler![
             medications::commands::create_medication,
